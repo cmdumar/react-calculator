82c2430f8f82f108d7f0622216492165d4717794
unknown <mohammadumar28@gmail.com>
2021-01-10 00:56:02 +0530

    Make the app a stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display result="0" />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display result="0" />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
