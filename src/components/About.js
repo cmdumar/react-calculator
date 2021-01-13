c117918462b3afc397192b7ed27f9288388b58bb
unknown <mohammadumar28@gmail.com>
2021-01-13 19:14:18 +0530

    Change about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => (
   <div className="homepage">
-    <h1>About</h1>
-    <p>This is a practice site for React Router.</p>
+    <p>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding. –William Paul Thurston
+    </p>
   </div>
 );
 
